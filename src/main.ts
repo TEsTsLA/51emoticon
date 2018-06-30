e418d12f7237766d95054de49ff569fefed22e05
chijian <1938531151@qq.com>
2018-06-30 14:44:35 +0800

    compression gitzip 和cookieParser 安装

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule,server);
   await app.use(express.static('public', { maxAge: 7 * 24 * 60 * 60 * 1000 }))
+  await app.use(compression());
+	await app.use(cookieParser());
   await app.listen(3001);
 } 
 bootstrap();
