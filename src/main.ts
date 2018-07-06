1fa71d3c85c76f71993e0af45665e0383dc9cee7
chijian <1938531151@qq.com>
2018-07-06 11:51:43 +0800

    cors 新方式

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,12 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const server = express();
-  const app = await NestFactory.create(AppModule, server);
+  const app = await NestFactory.create(AppModule, server, { cors: true });
   await app.use(express.static('public', { maxAge: 7 * 24 * 60 * 60 * 1000 }));
   await app.use(compression());
   await app.use(cookieParser());
-  await app.use(cors());
+  // app.enableCors();
+  // await app.use(cors());
   await app.listen(3001, '0.0.0.0', () => {});
 }
 bootstrap();
